a245f8557be6fc7c501460c687c1a1137df9174b
victorozzyy <victorozzyy@gmail.com>
2025-10-24 16:34:32 -0300

    Update m3u8.js

--- js/m3u8.js
+++ js/m3u8.js
@@ -215,22 +215,22 @@ document.addEventListener("DOMContentLoaded", () => {
       {
         name: "🔥 Minha Lista Principal",
         description: "Lista 01",
-        url: "http://felas87dz.icu/get.php?username=Anonymous100&password=Hacker100&type=m3u_plus"
+        url: "https://felas87dz.icu/get.php?username=Anonymous100&password=Hacker100&type=m3u_plus"
       },
       {
         name: "🔥 Minha 02",
         description: "Lista 02",
-        url: "http://felas87dz.icu/get.php?username=ednamaria&password=366242934&type=m3u_plus"
+        url: "https://felas87dz.icu/get.php?username=ednamaria&password=366242934&type=m3u_plus"
       },
       {
         name: "🔥 Minha 03",
         description: "Lista 03",
-        url: "http://felas87dz.icu/get.php?username=Diego01&password=9518484&type=m3u_plus"
+        url: "https://felas87dz.icu/get.php?username=Diego01&password=9518484&type=m3u_plus"
       },
       {
         name: "🔥 Minha Lista 04",
         description: "Lista 04",
-        url: "http://felas87dz.icu/get.php?username=854191413&password=383942274&type=m3u_plus"
+        url: "https://felas87dz.icu/get.php?username=854191413&password=383942274&type=m3u_plus"
       }
     ];
 
